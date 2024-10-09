98749ac040cbf2f62d5a2d036741d7f96bc0e505
sofikalpin <sofikalpin@hotmail.com>
2024-10-09 13:36:12 -0300

    Crud Alarma

--- src/app.mjs
+++ src/app.mjs
@@ -1,24 +1,29 @@
 // src/app.mjs
 import express from 'express';
-import vecindarioRoutes from './routes/vecindarioRoutes.mjs';
+import vecindarioRoutes from './routes/vecindarioRoutes.mjs'; 
+import alarmaRoutes from './routes/alarmaRoutes.mjs'; 
 import morgan from 'morgan';
-import globalErrorHandler from './middleware/globalErrorHandler.mjs';
+import globalErrorHandler from './middleware/globalErrorHandler.mjs'; 
 
 const app = express();
 
 // Middlewares
-app.use(express.json());
-app.use(morgan('dev'));
+app.use(express.json()); 
+app.use(morgan('dev')); 
 
 // Ruta básica para la raíz
 app.get('/', (req, res) => {
-	res.status(200).send('Bienvenido a la API de Vecindarios');
+    res.status(200).send('Bienvenido a la API de VegiNet');
 });
 
 // Rutas de vecindarios
-app.use('/api/vecindarios', vecindarioRoutes);
+app.use('/api/vecindarios', vecindarioRoutes); 
+
+// Rutas de alarmas
+app.use('/api/alarmas', alarmaRoutes); 
 
 // Middleware de manejo de errores
-app.use(globalErrorHandler);
+app.use(globalErrorHandler); 
 
-export default app;
+export default app; 
+
