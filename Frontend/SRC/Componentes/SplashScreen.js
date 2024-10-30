e195bcafae2232f1daf1fde1d529af524adbf715
sofikalpin <sofikalpin@hotmail.com>
2024-10-30 14:54:41 -0300

    Inicio APP

--- Frontend/SRC/Componentes/SplashScreen.js
+++ Frontend/SRC/Componentes/SplashScreen.js
@@ -1,28 +1,42 @@
-
 import React from 'react';
-import { View, Text, Image, StyleSheet, TouchableOpacity } from 'react-native';
+import { View, Text, Image, StyleSheet, TouchableOpacity, ImageBackground } from 'react-native';
 
 export default function SplashScreen({ navigation }) {
   return (
-    <View style={styles.container}>
-      <Image source={require('../../assets/logo.png')} style={styles.logo} />
-      <Text style={styles.title}>VigiNet</Text>
-      <Text style={styles.subtitle}>Comunidad segura</Text>
-      <TouchableOpacity
-        style={styles.button}
-        onPress={() => navigation.navigate('Login')}
-      >
-        <Text style={styles.buttonText}>INGRESAR</Text>
-      </TouchableOpacity>
-    </View>
+    <ImageBackground 
+      source={require('../../assets/Fondo.png')} 
+      style={styles.background}
+    >
+      <View style={styles.container}>
+        <Image source={require('../../assets/logo.png')} style={styles.logo} />
+        <Text style={styles.title}>VigiNet</Text>
+        <Text style={styles.subtitle}>Comunidad segura</Text>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => navigation.navigate('Login')}
+        >
+          <Text style={styles.buttonText}>INGRESAR</Text>
+        </TouchableOpacity>
+      </View>
+    </ImageBackground>
   );
 }
 
 const styles = StyleSheet.create({
-  container: { flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#E0F2F1' },
-  logo: { width: 100, height: 100, marginBottom: 20 },
-  title: { fontSize: 28, fontWeight: 'bold', color: '#333' },
-  subtitle: { fontSize: 16, color: '#333', marginBottom: 20 },
-  button: { backgroundColor: '#388E3C', paddingHorizontal: 40, paddingVertical: 10, borderRadius: 5 },
-  buttonText: { color: '#FFF', fontSize: 16 },
+  background: { 
+    flex: 1, 
+    justifyContent: 'center', 
+    alignItems: 'center', 
+  },
+
+  container: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  
+  logo: { width: 200, height: 210, marginBottom: 20 },
+  title: { fontSize: 58, fontWeight: 'bold', color: '#000000', textAlign: 'center' },
+  subtitle: { fontSize: 30, color: '#000000', marginBottom: 100, fontWeight: 'bold', textAlign: 'center' },
+  button: { backgroundColor: '#093916', paddingHorizontal: 40, paddingVertical: 20, borderRadius: 20 },
+  buttonText: { color: '#FFF', fontSize: 20, fontWeight: 'bold', textAlign: 'center' },
 });
