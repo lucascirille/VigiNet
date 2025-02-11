12a99e16cc9f86d1b765f50f25b22f183a183793
lucascirille <lucas.cirille@gmail.com>
2025-02-10 23:37:19 -0300

    Arreglo back alertas

--- Backend/src/routes/alarmaRoutes.mjs
+++ Backend/src/routes/alarmaRoutes.mjs
@@ -1,22 +1,19 @@
 import express from "express";
 import * as alarmaController from "../controllers/alarmaController.mjs";
-import { authenticateToken } from "../middleware/authMiddleware.mjs";
 
 
-export default (io) => {
-  const router = express.Router();
+const router = express.Router();
 
-  router
-    .route("/")
-    .get(alarmaController.getAllAlarmas)
-    .post(alarmaController.createAlarma);
+router
+  .route("/")
+  .get(alarmaController.getAllAlarmas)
+  .post(alarmaController.createAlarma);
 
-  router
-    .route("/:id")
-    .get(alarmaController.updateAlarma)
-    .delete(alarmaController.deleteAlarma);
+router
+  .route("/:id")
+  .get(alarmaController.updateAlarma)
+  .delete(alarmaController.deleteAlarma);
 
-  router.post("/activar", authenticateToken, (req, res) => activarAlarmaController(req, res, io));
 
-  return router;
-};
+export default router;
+
